b6636998bd4d7cf73704fa21543e12eaa9bc60a0
cnuebred <jonatann01ew@gmail.com>
2025-02-27 22:51:12 +0100

    init project - basic app server, tools, npm project

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { Pinup } from 'pinup'
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const app = express()
+const pinup = new Pinup(app, {
+    port: 3300,
+    auth: {
+        secret: process.env.BACKEND_AUTH_SECRET
+    },
+    logger: true,
+    logger_file: './server.log'
+})
+
+pinup.run({
+    print_setup_config: true
+})
