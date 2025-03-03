0c7af6372d4b80fe35e739258caf22ae8e7bf0c0
pspice <jonatann01ew@gmail.com>
2025-03-03 23:42:18 +0100

    fix bash variable

--- tools/release.sh
+++ tools/release.sh
@@ -11,8 +11,8 @@ fi
 
 version=$(cat ./package.json | grep -E '"version": "([0-9]+.[0-9]+.[0-9]+)"' | grep -Ei "[0-9]+.[0-9]+.[0-9]+" -o1) 
 
-
-echo "CHATMD_VERSION=${version}_${tag}${are_changes_token}" > .process
+chatmd_version="${version}_${tag}${are_changes_token}"
+echo "CHATMD_VERSION=${chatmd_version}" > .process
 
 ./tools/upload.sh ./.process
 
@@ -21,7 +21,7 @@ echo "CHATMD_VERSION=${version}_${tag}${are_changes_token}" > .process
 # ends up with add process file to git
 
 git add ./.process
-git commit -m"update process file - new release: $CHATMD_VERSION"
+git commit -m"update process file - new release: $chatmd_version"
 
 . ./.process
 
