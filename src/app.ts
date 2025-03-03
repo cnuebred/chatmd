3265a81f796c6c32a2f7d6107c6a81dcfefd1e3a
pspice <jonatann01ew@gmail.com>
2025-03-03 23:39:03 +0100

    update test release - todo full release; remote breakpoint

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,20 @@ import { Pinup } from 'pinup'
 import express from 'express'
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({ path: ['.process', '.env'] })
 
 
 const app = express()
 const pinup = new Pinup(app, {
-    port: 3300,
+    port: process.env.BACKEND_PORT,
     auth: {
         secret: process.env.BACKEND_AUTH_SECRET
     },
     logger: true,
     logger_file: './server.log'
 })
+
+console.log(`APP VERSION: ${process.env.CHATMD_VERSION}`)
 
 pinup.run({
     print_setup_config: true
--- tools/release.sh
+++ tools/release.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 
 tag=$(git rev-parse --short HEAD)
 are_changes=$(git status --porcelain)
@@ -12,5 +12,16 @@ fi
 version=$(cat ./package.json | grep -E '"version": "([0-9]+.[0-9]+.[0-9]+)"' | grep -Ei "[0-9]+.[0-9]+.[0-9]+" -o1) 
 
 
-echo "${version}_${tag}${are_changes_token}"
-# TODO
+echo "CHATMD_VERSION=${version}_${tag}${are_changes_token}" > .process
+
+./tools/upload.sh ./.process
+
+
+
+# ends up with add process file to git
+
+git add ./.process
+git commit -m"update process file - new release: $CHATMD_VERSION"
+
+. ./.process
+
